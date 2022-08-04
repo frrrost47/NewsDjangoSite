b357d2fe0f652ab812b4ad07a38a7483b40dd92e
frrrost <depositvs@yandex.ru>
2022-08-04 03:21:27 -0400

    Fix  git problems

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -6,5 +6,6 @@ class News(models.Model):
     content = models.TextField(blank=True)  # blank true - не обязательно к заполнению
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    ''' Для использования ImageField django при старте сервера заставит установить доп библиотеку'''
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')  # путь загрузки картинок
     is_published = models.BooleanField(default=True)  # без default = none
