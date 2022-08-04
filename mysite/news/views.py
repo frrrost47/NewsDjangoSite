c5c978cfa3486b19a526a4e13ce3e9351c1ab6e0
frrrost <depositvs@yandex.ru>
2022-08-04 02:22:28 -0400

    News get url patterns

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,4 +1,13 @@
 """View controllers"""
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(request):
+    print(dir(request))
+    return HttpResponse('Hello world')
+
+
+def test(request):
+    return HttpResponse('Тестовая страница')
