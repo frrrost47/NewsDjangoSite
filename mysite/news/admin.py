505c3e05b8794fae4f4c1f2a82f0d99e11fe3708
frrrost <depositvs@yandex.ru>
2022-08-05 03:01:37 -0400

    Add new model with reference one to many

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Category
 
 
 # admin.site.register(News)
@@ -7,7 +7,20 @@ from .models import News
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')  # поля, которые будут видны в админке
-    list_display_links = ('id', 'title')  # поля, которые ссылаются на редактирование модели в админке
-    search_fields = ('title', 'content')  # поля по которым будет работать поиск в админке (регистр ру поиска важен)
+    # поля, которые будут видны в админке
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    # поля, которые ссылаются на редактирование модели в админке
+    list_display_links = ('id', 'title')
+    # поля по которым будет работать поиск в админке (регистр ру поиска важен)
+    search_fields = ('title', 'content')
+    # поля которые редактируются прям из списка админки
+    list_editable = ('is_published',)
+    # добавит фильтр по полям в админке
+    list_filter = ('is_published', 'category')
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -7,19 +7,20 @@ class News(models.Model):
     content = models.TextField(blank=True, verbose_name='Контент')  # blank true - не обязательно к заполнению
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    ''' 
-    1) Для использования ImageField django при старте сервера заставит установить 
+    ''' 1) Для использования ImageField django при старте сервера заставит установить 
     доп библиотеку Pillow которая проверяет файлы на изображения.
     2) Придется добавить в settings.py такие свойства:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     MEDIA_URL = '/media/' 
     3) Для отладочного режима придется добавить в urls.py "if settings.DEBUG:" где будут использоваться эти 
-    константы. Без отладочного режима сервер сам все сделает. 
-    '''
+    константы. Без отладочного режима сервер сам все сделает. '''
     # путь загрузки картинок из админки
-    # blank=Trui - опционально для заполнения
+    # blank=True - опционально для заполнения
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')  # без default = none
+
+    ''' Опциональная связь "многие к одному" '''
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     # в shell принтит объект отображая его title
     def __str__(self):
@@ -31,3 +32,16 @@ class News(models.Model):
         verbose_name_plural = 'Новости'  # наименование модели во множественном числе
         ordering = ['-created_at']
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    # в shell принтит объект отображая его title
+    def __str__(self):
+        return self.title
+
+    # класс для визуализации админки и пользовательской части
+    class Meta:
+        verbose_name = 'Категория'  # наименование модели в единственном числе
+        verbose_name_plural = 'Категории'  # наименование модели во множественном числе
+        ordering = ['title']
