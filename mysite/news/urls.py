4c5a5b3b1cf646ef7e94aedd629447957547bd31
frrrost <depositvs@yandex.ru>
2022-08-08 03:57:34 -0400

    End lesson 18. Add global url for templates

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('category/<int:category_id>/', get_category),  # URL с динамическим id
+    path('', index, name='home'),  # name - имя маршрута(используется чтобы при смене маршрута не менять все шаблоны)
+    path('category/<int:category_id>/', get_category, name='category'),  # URL с динамическим id
 ]
