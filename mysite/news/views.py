4ff16c6dec510e1ecb0f3ed86f1de11530a4424b
frrrost <depositvs@yandex.ru>
2022-08-08 03:32:49 -0400

    End lesson 17. Add category template.

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,14 +1,24 @@
 """View controllers"""
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'Список новостей'
+        'title': 'Список новостей',
+        'categories': categories,
     }
     # шаблон ищется в папке templates по умолчанию
     return render(request, 'news/index.html', context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {'news': news, 'categories': categories, 'category': category}
+    return render(request, 'news/category.html', context)
