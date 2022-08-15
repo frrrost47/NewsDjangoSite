2eb0c09965c7743046c4dee78ab73e3a9458b93d
frrrost <depositvs@yandex.ru>
2022-08-15 03:47:00 -0400

    End lesson 30. End testing view controllers generic

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,7 +2,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import News, Category
 from .forms import NewsForm
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 
 # Все новости
@@ -10,6 +11,7 @@ class HomeNews(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'  # название объектов БД для шаблона
+
     # дополнительные атрибуты для шаблона(не рекомендуется для динамичных данных)
     # extra_context = {'title': 'Главная'}
 
@@ -48,40 +50,46 @@ class ViewNews(DetailView):
     # py_url_kwarg = 'news_id'
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-    context = {'news': news, 'category': category}
-    return render(request, 'news/category.html', context)
+class CreateNews(CreateView):
+    form_class = NewsForm  # связываем класс с формой
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')  # redirect url
 
 
-def view_news(request, news_id):
-    #  news_item = News.objects.get(pk=news_id)
-    """ если не будет новости по pk, то выскочит исключение 404
-    сокращенная запись от
-    from django.http import Http404
-    try:
-        obj = MyModel.objects.get(pk=1)
-    except MyModel.DoesNotExist:
-        raise Http404('No MyModel matches the given query.') """
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item})
-
-
-# Контроллер с формой, которая связанна с моделью
-def add_news(request):
-    if request.method == 'POST':
-        # таким образом заполнили форму через POST
-        form = NewsForm(request.POST)
-        # если форма прошла валидацию
-        if form.is_valid():
-            # form.cleaned_data - данные с которыми можно работать(например для SQL запросов)
-            news = form.save()
-            # после сохранения перекинут на страницу новости
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     context = {'news': news, 'category': category}
+#     return render(request, 'news/category.html', context)
+#
+#
+# def view_news(request, news_id):
+#     #  news_item = News.objects.get(pk=news_id)
+#     """ если не будет новости по pk, то выскочит исключение 404
+#     сокращенная запись от
+#     from django.http import Http404
+#     try:
+#         obj = MyModel.objects.get(pk=1)
+#     except MyModel.DoesNotExist:
+#         raise Http404('No MyModel matches the given query.') """
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {'news_item': news_item})
+#
+#
+# # Контроллер с формой, которая связанна с моделью
+# def add_news(request):
+#     if request.method == 'POST':
+#         # таким образом заполнили форму через POST
+#         form = NewsForm(request.POST)
+#         # если форма прошла валидацию
+#         if form.is_valid():
+#             # form.cleaned_data - данные с которыми можно работать(например для SQL запросов)
+#             news = form.save()
+#             # после сохранения перекинут на страницу новости
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})
 
 
 ''' Контроллер с формой, которая НЕ связанна с моделью'''
