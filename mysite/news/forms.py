a3ce2978a7a820d39fa2d8fc1e6d78afdce202dc
frrrost <depositvs@yandex.ru>
2022-09-08 03:58:42 -0400

    End lesson 46. 2\2 part. User registration

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -2,6 +2,27 @@ from django import forms
 from .models import News
 import re
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label='Имя пользователя', help_text='Максимум 150 символов',
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='E-mail', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        # Виджеты добавляют атрибуты на поля формы меняя внешний вид
+        # widgets = {
+        #     'username': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'email': forms.EmailInput(attrs={'class': 'form-control', 'rows': '5'}),
+        #     'password1': forms.PasswordInput(attrs={'class': 'form-control', 'rows': '5'}),
+        #     'password2': forms.PasswordInput(attrs={'class': 'form-control', 'rows': '5'}),
+        # }
 
 
 class NewsForm(forms.ModelForm):
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,20 +1,20 @@
 """View controllers"""
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import News, Category
-from .forms import NewsForm
+from .forms import NewsForm, UserRegisterForm
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from .utils import MyMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
-from django.contrib.auth.forms import UserCreationForm
+
 from django.contrib import messages
 
 
 # регистрация нового пользователя
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Вы успешно зарегистрировались')
@@ -22,7 +22,7 @@ def register(request):
         else:
             messages.error(request, 'Ошибка регистрации')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'news/register.html', {'form': form})
 
 
